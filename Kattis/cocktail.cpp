cf91fc017110a2143cb13c5041973308edc569f1
Liam Jay <145520385+liamslj13@users.noreply.github.com>
2025-03-12 01:34:09 -0300

    Add files via upload

--- Kattis/cocktail.cpp
+++ Kattis/cocktail.cpp
@@ -1,1 +1,21 @@
+// timed mode
+#include <bits/stdc++.h>
+using namespace std;
 
+
+
+int main() {
+  // n -> # of friends, k -> # of days until bday, d -> today
+  int n,k,d,q; // q -> quarantine
+  cin>>n>>k>>d;
+
+  int cc = 0;
+  for (int i = 0; i < n; i++) {
+    cin>>q;
+    if (q <= d) {
+      cc++;
+    }
+  }
+
+  return cc;
+}
